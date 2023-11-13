724b9f51e1acc607fe29131de063cc24cb613728
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-11-13 22:25:58 +0300

    2nd part of calender

--- app/duty/[duty]/page.tsx
+++ app/duty/[duty]/page.tsx
@@ -1,20 +1,22 @@
-import { kv } from "@vercel/kv";
+import Calendar from "@/components/Calendar";
 import Link from "next/link";
 
-function getDayInMoth(month: number, year: number) {
-  const date = new Date(year, month, 1);
-  const days = [];
-  while (date.getMonth() == month) {
-    days.push(new Date(date));
-    date.setDate(date.getDate() + 1);
-  }
-  return days;
-}
+// function getDayInMoth(month: number, year: number) {
+//   const date = new Date(year, month, 1);
+//   const firstWeekDay = (date.getDay() +6) % 7
+//   const numberOfEmptyDays = Array(firstWeekDay).fill(null)
+//   const days = [...numberOfEmptyDays];
+//   while (date.getMonth() == month) {
+//     days.push(new Date(date));
+//     date.setDate(date.getDate() + 1);
+//   }
+//   return days;
+// }
 
-const currentDate = new Date();
-const currentDay = currentDate.getDate();
-const currentMonth = currentDate.getMonth();
-const currentYear = currentDate.getFullYear();
+// const currentDate = new Date();
+// const currentDay = currentDate.getDate();
+// const currentMonth = currentDate.getMonth();
+// const currentYear = currentDate.getFullYear();
 
 export default async function Duty({
   params: { duty },
@@ -22,9 +24,7 @@ export default async function Duty({
   params: { duty: string };
 }) {
   const decodedDuty = decodeURI(duty);
-  const weekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-
-  const daysInMonth = getDayInMoth(currentMonth, currentYear);
+  
   return (
     <main className="container relative flex flex-col gap-8 px-12 pt-16">
       <h1 className=" text-2xl font-light text-center text-white font-display">
@@ -34,7 +34,8 @@ export default async function Duty({
         ⬿Back
       </Link>
 
-      <section className="w-full my-2 rounded-md bg-neutral-800">
+        <Calendar />
+      {/* <section className="w-full my-2 rounded-md bg-neutral-800">
         <div className="flex justify-between mx-2 my-4 font-sans text-neutral-400">
           <button>←</button>
           <span>November</span>
@@ -50,14 +51,14 @@ export default async function Duty({
           ))}
 
           {daysInMonth.map((day, index) => (
-            <div key={index}>
-              <span className="font-sans text-xs font-light text-neutral-400">
-                {day.getDate()}
+            <div key={index} className="flex flex-col items-center p-2">
+              <span className="font-sans text-xs text-center font-light text-neutral-400">
+                {day?.getDate()}
               </span>
             </div>
           ))}
         </div>
-      </section>
+      </section> */}
     </main>
   );
 }
---
+++ components/Calendar.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+function getDayInMonth(month: number, year: number) {
+  const date = new Date(year, month, 1);
+  const firstWeekDay = (date.getDay() + 6) % 7;
+  const numberOfEmptyDays = Array(firstWeekDay).fill(null);
+  const days = [...numberOfEmptyDays];
+  while (date.getMonth() == month) {
+    days.push(new Date(date));
+    date.setDate(date.getDate() + 1);
+  }
+  return days;
+}
+
+const currentDate = new Date();
+const currentMonth = currentDate.getMonth();
+const currentYear = currentDate.getFullYear();
+const weekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+export default function Calendar() {
+  const [month, setMonth] = useState(currentMonth);
+  const [year, setYear] = useState(currentYear);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [daysInMonth, setDaysInMonth] = useState(
+    getDayInMonth(currentMonth, currentYear)
+  );
+
+  useEffect(() => {
+    setDaysInMonth(getDayInMonth(month, year));
+    setSelectedDate(new Date(year, month));
+  }, [month, year]);
+
+  function goToPreviousMonth() {
+    if (month === 0) {
+      setYear(year - 1);
+      setMonth(11);
+    } else {
+      setMonth(month - 1);
+    }
+  }
+  function goToNextMonth() {
+    if (month === 11) {
+      setYear(year + 1);
+      setMonth(0);
+    } else {
+      setMonth(month + 1);
+    }
+  }
+
+  function getFullDate() {
+    return `${selectedDate.toLocaleString("en", {
+      month: "long",
+    })} of ${selectedDate.getFullYear()}`;
+  }
+  return (
+    <section className="w-full my-2 rounded-md bg-neutral-800">
+      <div className="flex justify-between mx-2 my-4 font-sans text-neutral-400">
+        <button onClick={goToPreviousMonth}>←</button>
+        <span>{getFullDate()}</span>
+        <button onClick={goToNextMonth}>→</button>
+      </div>
+      <div className="grid w-full grid-cols-7 mt-2">
+        {weekDays.map((day) => (
+          <div className="flex flex-col items-center p-2" key={day}>
+            <span className="font-sans text-xs font-light text-neutral-200">
+              {day}
+            </span>
+          </div>
+        ))}
+
+        {daysInMonth.map((day, index) => (
+          <div key={index} className="flex flex-col items-center p-2">
+            <span className="font-sans text-xs text-center font-light text-neutral-400">
+              {day?.getDate()}
+            </span>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
